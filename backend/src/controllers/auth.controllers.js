0ab041d0b9ebad0fa4855c25f288cbe0af65f0e0
YazanAlshoubakii <yazan.h.alshoubaki@gmail.com>
2024-11-27 23:29:33 +0300

    update profile controller

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -1,6 +1,7 @@
 import { generateToken } from '../lib/utils.js';
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
+import cloudinary from '../lib/cloudinary.js';
 
 export const signupController = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -83,4 +84,25 @@ export const logoutController = (req, res) => {
   }
 };
 
-export const updateProfile = async (req, res) => {};
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+    const userId = req.user._id;
+    if (!profilePic) {
+      return res.status(400).json({ msg: 'Profile Picture is required' });
+    }
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        profilePic: uploadResponse.secure_url,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log('error in update profile ', error);
+    res.status(500).json({ msg: 'Internal server error' });
+  }
+};
