16dcc7ba24a5d23b1fa494e0b9619cc71aabe6a1
YazanAlshoubakii <yazan.h.alshoubaki@gmail.com>
2024-11-28 01:27:02 +0300

    add getMessage and sendMessage controller

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,4 +1,5 @@
 import User from '../models/user.model.js';
+import Message from '../models/message.model.js';
 
 export const getUsersForSidebar = async (req, res) => {
   try {
@@ -11,5 +12,58 @@ export const getUsersForSidebar = async (req, res) => {
   } catch (error) {
     console.log('Error in getUsersForSidebar controller ', error.message);
     res.status(500).json({ error: 'Internal server Error' });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const myId = req.user._id;
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
+      ],
+    });
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log('Error in getMessages Controller ', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let imageUrl;
+    if (imageUrl) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+
+    // todo: realTime functionality goes here => socket.io
+
+    res.status(201).json({
+      senderId: myId,
+      receiverId: receiverId,
+      text,
+      image,
+    });
+  } catch (error) {
+    console.log('Error in sendMessages Controller ', error.message);
+    res.status(500).json({ error: 'Internal server error' });
   }
 };
