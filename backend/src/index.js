b9b1fb7a873a7907b77583dcf34e2861bc079a27
YazanAlshoubakii <yazan.h.alshoubaki@gmail.com>
2024-11-28 00:41:14 +0300

    add message route and getUsersForSidebar controller

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
 
 dotenv.config();
 const app = express();
@@ -13,6 +14,7 @@ app.use(cookieParser());
 const PORT = process.env.PORT;
 
 app.use('/api/auth', authRoutes);
+app.use('/api/message', messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${5001}`);
