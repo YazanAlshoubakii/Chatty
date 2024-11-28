d5bebe660c943144d473ab20ea24e8b019378880
YazanAlshoubakii <yazan.h.alshoubaki@gmail.com>
2024-11-28 22:15:50 +0300

    setting up cors in the backend and setting up authStore using zustand

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
@@ -9,8 +10,15 @@ import messageRoutes from './routes/message.route.js';
 dotenv.config();
 const app = express();
 
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
+
 const PORT = process.env.PORT;
 
 app.use('/api/auth', authRoutes);
