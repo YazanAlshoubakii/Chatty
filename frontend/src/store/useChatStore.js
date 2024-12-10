dac4a0fd891daaf7e066d565b1cc73a20a0eb061
YazanAlshoubakii <yazan.h.alshoubaki@gmail.com>
2024-12-11 00:40:46 +0300

    finish working on socket and add send message on time and add online filtered users , and create a bunch of users for testing

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -1,6 +1,7 @@
 import { create } from 'zustand';
 import toast from 'react-hot-toast';
 import { axiosInstance } from '../lib/axios';
+import { useAuthStore } from './useAuthStore.js';
 
 export const useChatStore = create((set, get) => ({
   messages: [],
@@ -51,6 +52,26 @@ export const useChatStore = create((set, get) => ({
     }
   },
 
-  // todo: Optimize this later
+  subscribeToMessages: () => {
+    const { selectedUser } = get();
+    if (!selectedUser) return;
+
+    const socket = useAuthStore.getState().socket;
+
+    socket.on('newMessage', (newMessage) => {
+      const isMessageSentFromSelectedUser =
+        newMessage.senderId === selectedUser._id;
+      if (!isMessageSentFromSelectedUser) return;
+      set({
+        messages: [...get().messages, newMessage],
+      });
+    });
+  },
+
+  unsubscribeFromMessages: () => {
+    const socket = useAuthStore.getState().socket;
+    socket.off('newMessage');
+  },
+
   setSelectedUser: (selectedUser) => set({ selectedUser }),
 }));
