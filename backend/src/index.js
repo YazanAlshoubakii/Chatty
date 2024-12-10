a2e7d9e02cac2795e70f4ac3e4a743d10b14844a
YazanAlshoubakii <yazan.h.alshoubaki@gmail.com>
2024-12-11 01:13:32 +0300

    setting up to procution phase

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,8 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { app, server } from './lib/socket.js';
 
+import path from 'path';
+
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 
@@ -16,13 +18,24 @@ app.use(
     credentials: true,
   })
 );
-app.use(express.json());
-app.use(cookieParser());
 
 const PORT = process.env.PORT;
 
+const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(cookieParser());
+
 app.use('/api/auth', authRoutes);
 app.use('/api/message', messageRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${5001}`);
